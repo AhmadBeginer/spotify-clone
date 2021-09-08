8e08722e3807ed32ce4d8782d297224928a34179
AhmadBeginer <rajaa6646@gmail.com>
2021-09-08 20:13:38 +0530

    added new one

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,14 +1,20 @@
-const BlogList = ({ blogs }) => {
+import { Link } from "react-router-dom";
+
+const BlogList = ({ blogs,title }) => {
   return (
     <div className="blog-list">
-      {blogs.map(blog => (
-        <div className="blog-preview" key={blog.id} >
-          <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{title}</h2>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+          </Link>
         </div>
       ))}
     </div>
   );
-}
- 
+};
+
+
 export default BlogList;
--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
+
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
+  const { error, isPending, data: blogs } = useFetch('http://localhost:9000/blogs')
 
   return (
     <div className="home">
